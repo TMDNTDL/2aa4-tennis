5f813044c65476f21404c3a7cb403ffa8174d449
TMDNTDL <zhicong800@gmail.com>
2024-01-17 20:20:27 -0500

    add skeleton of scoresystem

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
@@ -1,13 +1,26 @@
 package ca.mcscert.se2aa4.demos.tennis;
 
+import java.util.Optional;
+
 public class Match {
     public static final String P1_NAME = "P1";
-    public static final String P2_NAME = "P1";
+    public static final String P2_NAME = "P2";
     public Match(Integer p1Strength, Integer p2Strength){
 
     }
     public String play(){
-        return "No winner... yet!";
+        ScoreSystem scorer = new ScoreSystem();
+        while(!scorer.isEnded()){
+            String who = decideGameWinner();
+            scorer.score(who);
+        }
+        return scorer.winner().
+                orElseThrow(() -> new IllegalArgumentException("No Winner!"));
+    }
+
+    private String decideGameWinner(){
+        System.out.println("This player wins the game: " + P1_NAME);
+        return P1_NAME;
     }
 
 }
